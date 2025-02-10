86756580968df853a59f01d737cc951f088b1cc2
deva3xd <devarn05@gmail.com>
2025-02-11 00:58:10 +0700

    fix(button): behind picture

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Arrow from "../assets/icon/up-arrow.png";
 
 const Button = () => {
   return (
-    <a href='#beranda' className="fixed bottom-0 right-0 m-5 bg-custom-primary p-4 border border-white rounded-full hover:bg-gray-500"><img src={Arrow} alt="Arrow" /></a>
+    <a href='#beranda' className="fixed bottom-0 right-0 m-5 bg-custom-primary p-4 border border-white rounded-full hover:bg-gray-500 z-50"><img src={Arrow} alt="Arrow" /></a>
   )
 }
 
