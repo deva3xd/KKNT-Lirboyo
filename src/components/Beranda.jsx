e3247955f5f423f2e13847b16d3cf696e6f7d7bb
deva3xd <devarn05@gmail.com>
2025-02-18 14:01:10 +0700

    fix button up

--- src/components/Beranda.jsx
+++ src/components/Beranda.jsx
@@ -4,13 +4,13 @@ import Profil from '../assets/image/profil.png';
 const Beranda = () => {
   return (
     <div className='beranda bg-black bg-opacity-50 relative' id='beranda'>
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-35 z-10 flex flex-col sm:flex-row justify-center items-center px-5">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-55 z-10 flex flex-col sm:flex-row justify-center items-center px-5">
         <div className='w-full sm:w-1/2 flex justify-center pt-12 sm:pt-0'>
-          <img src={Profil} alt='profil' className='sm:w-9/12' />
+          <img src={Profil} alt='profil' className='w-8/12' />
         </div>
         <div className='w-full sm:w-1/2 font-extrabold text-center sm:text-end'>
-          <h1 className='banthern text-2xl lg:text-4xl' style={{ color: '#933e01', WebkitTextStrokeWidth: '0.1px', WebkitTextStrokeColor: 'white'  }}>Budaya Kesenian<br /><span className='banthern text-4xl lg:text-6xl'>Wayang Jemblung</span></h1>
-          <p className='text-base lg:text-xl text-black' style={{ WebkitTextStrokeWidth: '0.5px', WebkitTextStrokeColor: 'white' }}>Salah satu kesenian tradisional <br /> Jawa Timur terkhusus wilayah Kediri<br />dan sekitarnya.</p>
+          <h1 className='banthern text-2xl lg:text-4xl text-white'>Budaya Kesenian<br /><span className='banthern text-4xl lg:text-6xl underline'>Wayang Jemblung</span></h1>
+          <p className='text-sm lg:text-lg text-white'>Salah satu kesenian tradisional <br /> Jawa Timur terkhusus wilayah Kediri<br />dan sekitarnya.</p>
         </div>
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Arrow from "../assets/icon/up-arrow.png";
 
 const Button = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+
+      if (position > window.innerHeight) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <a href='#beranda' className="fixed bottom-0 right-0 m-5 bg-custom-primary p-3 border border-white rounded-full hover:bg-orange-800 z-50"><img src={Arrow} alt="Arrow" /></a>
-  )
-}
+    isVisible && (
+      <a href="#beranda" className="fixed bottom-0 right-0 m-5 bg-custom-primary p-3 border border-white rounded-full hover:bg-orange-800 z-50">
+        <img src={Arrow} alt="Arrow" />
+      </a>
+    )
+  );
+};
 
 export default Button;
